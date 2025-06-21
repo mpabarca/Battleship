30a0ea654ed27e208716b39665e5db3da48fa200
mpabarca <mp.abarca.alvear@gmail.com>
2025-06-21 12:01:43 +0200

    Create row/column border guidelines on grid layout (battleship #4)

--- src/components/ui/Grid/index.tsx
+++ src/components/ui/Grid/index.tsx
@@ -1,17 +1,56 @@
 import type { GridType } from "../../../types";
 import { generateGrid, GRID_SIZE } from "../../../utils/game";
+import { ALPHABET, getConsecutivesIntArrayBySize } from "../../../utils/general";
 import Cell from "../Cell";
 
 function Grid () {
   const grid: GridType = generateGrid()
-  const [columns, rows] = GRID_SIZE;
+  const columnsHeader: number[] = getConsecutivesIntArrayBySize(GRID_SIZE[0])
 
   return(
-    <div className="border border-zinc-600 p-14">
-      <div className={`grid gap-0 grid-rows-${rows} border border-zinc-600`}>
-        {grid.layout.map((row, rowIndex) => (
-          <div key={rowIndex} className={`grid gap-0 grid-cols-${columns}`}>
-            {row.map((cell, columnIndex) => (<Cell key={columnIndex} cell={cell} />))}
+    <div className="flex border border-zinc-600 p-14">
+      <div>
+        <div className="w-14 h-14">
+          {""}
+        </div>
+        {grid.layout.map((_, rowIndex) => (
+          <div key={`${rowIndex}-header`} className="h-14 w-14 flex justify-center items-center">
+            {ALPHABET[rowIndex]}
+          </div>
+          ))
+        }
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          {columnsHeader.map((col) => (
+            <div key={`${col}-header`} className="w-14 h-14 flex justify-center items-center">
+              {col}
+            </div>
+          ))}
+        </div>
+        <div className="grid grid-rows-10 border border-zinc-600">
+          {grid.layout.map((row, rowIndex) => (
+            <div key={rowIndex} className={`grid gap-0 grid-cols-10`}>
+              {row.map((cell, columnIndex) => (<Cell key={columnIndex} cell={cell} />))}
+            </div>
+            ))
+          }
+        </div>
+        <div className="flex flex-row">
+          {columnsHeader.map((col) => (
+            <div key={`${col}-header`} className="w-14 h-14 flex justify-center items-center">
+              {col}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <div className="w-14 h-14">
+          {""}
+        </div>
+        {grid.layout.map((_, rowIndex) => (
+          <div key={`${rowIndex}-header`} className="h-14 w-14 flex justify-center items-center">
+            {ALPHABET[rowIndex]}
           </div>
           ))
         }
---
+++ src/components/ui/Input/index.tsx
@@ -0,0 +1,9 @@
+
+
+function Input (){
+  return(
+    <input placeholder="eg. A5" className="w-14 h-10 border-b-2 place-content-center"/>
+  )
+}
+
+export default Input;
