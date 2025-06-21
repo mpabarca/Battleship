92ab2e7e752b522179b0f657b0c5f0567174bd6e
mpabarca <mp.abarca.alvear@gmail.com>
2025-06-22 01:38:53 +0200

    Persist grid using localStorage (battleship #11)

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import Grid from "../ui/Grid"
-import FireControl from "../ui/FireControl"
+import { useEffect, useState } from "react";
+import Grid from "../ui/Grid";
+import FireControl from "../ui/FireControl";
 import type { CoordinatesType, GridType } from "../../types";
 import { generateGrid, getShotResult } from "../../utils/game";
 import { hasCellBeenShot } from "../../utils/cell";
@@ -10,40 +10,73 @@ import { hasCellBeenShot } from "../../utils/cell";
 /*
 Handling Data accross project:
 - GLOBAL => grid: GridType
-              |-> stored initially on localStorage (persistance) -> TEMPORARY STORED AS STATE
-              |-> only changes when button  inside <FireControl /> is click
+            ✔ |-> stored initially on localStorage (persistance)
+            ✔ |-> only changes when button  inside <FireControl /> is click
 - GLOBAL => [x,y] input <- from <FireControl />
             ✔ |-> stored as state on <Game /> parent component
               |-> <Grid /> reactively uses to show selected coordinates (constant visual feedback to user)
-              |-> when click button inside <FireControl /> is use to handle <grid> 
+            ✔ |-> when click button inside <FireControl /> is use to handle <grid> 
+
+Why use useState as state managment instead of react context or others?
+Based on scope, we have minimal global state: just grid and target. Which are fully controlled within the Game parent component.
+So are only need it at one level (or passed once to children).
 */
 
 function Game() {
   const [target, setTarget] = useState<CoordinatesType>();
-  const [grid, setGrid] = useState<GridType>()
+  const [grid, setGrid] = useState<GridType>();
 
-  function handleFire (): void {
-    if(target && grid && hasCellBeenShot(target, grid?.layout)) console.log("cell has been shot previously!")
-    if(target && grid) setGrid(getShotResult(grid, target))
+  function handleFire(): void {
+    if (target && grid && hasCellBeenShot(target, grid?.layout))
+      console.log("cell has been shot previously!");
+    if (target && grid) setGrid(getShotResult(grid, target));
   }
+
+  function resetGame() {
+    localStorage.removeItem("battleship-grid");
+    setGrid(generateGrid());
+  }
+
+  useEffect(() => {
+    const storedGrid = localStorage.getItem("battleship-grid");
+    if (storedGrid) {
+      setGrid(JSON.parse(storedGrid));
+    } else {
+      setGrid(generateGrid());
+    }
+  }, []);
+
+  useEffect(() => {
+    if (grid) {
+      localStorage.setItem("battleship-grid", JSON.stringify(grid));
+    }
+  }, [grid]);
 
   return (
     <>
-      {grid ? 
+      {grid ? (
         <div className='flex flex-row h-full gap-20'>
           <Grid grid={grid} />
-          <div className="flex flex-col h-full gap-20 justify-between">
-            <button type="button" onClick={() => setGrid(generateGrid())}>Reset game</button>
-            {grid.endGame ? <div>END GAME</div> : <FireControl setTarget={setTarget} handleFire={handleFire} />}
+          <div className='flex flex-col h-full gap-20 justify-between'>
+            <button type='button' onClick={resetGame}>
+              Reset game
+            </button>
+            {grid.endGame ? (
+              <div>END GAME</div>
+            ) : (
+              <FireControl setTarget={setTarget} handleFire={handleFire} />
+            )}
           </div>
         </div>
-        : 
+      ) : (
         <div className='flex flex-row items-center gap-20'>
-          <button type="button" onClick={() => setGrid(generateGrid())}>Start game</button>
+          <button type='button' onClick={() => setGrid(generateGrid())}>
+            Start game
+          </button>
         </div>
-      }
+      )}
     </>
-  )
+  );
 }
 
 export default Game;
