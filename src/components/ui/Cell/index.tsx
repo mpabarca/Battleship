5807d46e73cdd50f8e1cdcfeb84a4cc08a42390f
mpabarca <mp.abarca.alvear@gmail.com>
2025-06-23 02:15:10 +0200

    Add reactive feedback of target (battleship #19)

--- src/components/ui/Cell/index.tsx
+++ src/components/ui/Cell/index.tsx
@@ -1,26 +1,44 @@
-import { FlameKindling, Minus, Ship } from "lucide-react";
+import { Crosshair, FlameKindling, Minus, Ship } from "lucide-react";
 import type { CellType } from "../../../types";
 import { cn } from "@/utils/general";
+import type { SelectColumnType, SelectRowType } from "@/components/Game";
 
 interface ICell {
   cell: CellType;
   showShips: boolean;
+  selectingColumn: SelectColumnType;
+  selectingRow: SelectRowType;
 }
 
-function Cell({ cell, showShips }: ICell) {
+function Cell({ cell, showShips, selectingColumn, selectingRow }: ICell) {
+  const couldBeSelected =
+    (selectingColumn.selecting &&
+      selectingColumn.column === cell.coordinates[0]) ||
+    (selectingRow.selecting && selectingRow.row === cell.coordinates[1]);
+  const isBeenSelected =
+    selectingColumn.selecting &&
+    selectingColumn.column === cell.coordinates[0] &&
+    selectingRow.selecting &&
+    selectingRow.row === cell.coordinates[1];
+
   const bgColor = cell.shot
     ? cell.shipId
       ? cell.shipSunk
         ? "bg-black"
         : "bg-red-700"
       : "bg-orange-400/60"
+    : isBeenSelected
+    ? "bg-yellow-400"
+    : couldBeSelected
+    ? "bg-yellow-400/30"
     : "bg-transparent";
+
   return (
     <div
       id={`${cell.cellId}`}
       key={cell.cellId}
       className={cn(
-        "w-12 h-12 flex justify-center items-center border border-zinc-600 rounded-lg shadow-[3px_3px_0_0_black]",
+        "w-12 h-12 flex justify-center items-center border border-zinc-600 rounded-lg shadow-[3px_3px_0_0_black] transition-colors duration-400",
         bgColor
       )}
     >
@@ -34,6 +52,8 @@ function Cell({ cell, showShips }: ICell) {
         )
       ) : cell.shot ? (
         <Minus className='text-white' />
+      ) : isBeenSelected ? (
+        <Crosshair className='text-black' />
       ) : (
         ""
       )}
